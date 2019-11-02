0e6669860f8a8dea4c74dca5d1aa93b7f16203fc
AJ Guiles <ashiguiles@gmail.com>
2019-11-02 19:11:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-num1 = 2
-num2 = 31
+// num1 = 2
+// num2 = 31
 
-const multiple = num1*num2 
+// const multiple = num1*num2 
 
-const random = Math.floor( Math.random() * 10 ) + 1
+// const random = Math.floor( Math.random() * 10 ) + 1
 
-const mod = 16 % 6
+// const mod = 16 % 6
 
-const  max = Math.max( 2, 10,18 , 20 )
+// const  max = Math.max( 2, 10,18 , 20 )
 
