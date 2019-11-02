c377df9786417c06654c640c04e127ae4768a9d1
AJ Guiles <ashiguiles@gmail.com>
2019-11-02 19:11:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@
 
 // const  max = Math.max( 2, 10,18 , 20 )
 
+const var1 = 2
+const var2 = 31
+const multiply = var1 * var2
+const random = Math.floor( Math.random() * 1000 ) + 1
+const mod = 16 % 6
+const max = Math.max( 5, 10, 15, 20 )
+
