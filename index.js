b87004c29af0c0ec042ab3dbcb464722ce71725a
AJ Guiles <ashiguiles@gmail.com>
2019-11-02 19:10:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 num1 = 2
 num2 = 31
 
-let multiple = num1*num2 
+const multiple = num1*num2 
 
-let random = Math.floor( Math.random() * 10 ) + 1
+const random = Math.floor( Math.random() * 10 ) + 1
 
-let mod = 16 % 6
+const mod = 16 % 6
 
-let max = Math.max( 2, 10,18 , 20 )
+const  max = Math.max( 2, 10,18 , 20 )
 
